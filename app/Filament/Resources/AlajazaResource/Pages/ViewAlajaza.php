9363205b18764bcb9dbab2c0dabcb425d752e1e5
Your Name <you@example.com>
2022-09-17 20:04:37 +0200

    a

---
+++ app/Filament/Resources/AlajazaResource/Pages/ViewAlajaza.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\AlajazaResource\Pages;
+
+use App\Filament\Resources\AlajazaResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAlajaza extends CreateRecord
+{
+    protected static string $resource = AlajazaResource::class;
+}
